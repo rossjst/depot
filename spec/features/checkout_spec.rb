61726324ca9f899bf711437fa44762b4b6f4d43e
Terence Ponce <terence.ponce@promisepay.com>
2017-08-14 12:42:37 +0800

    Add specs for cart improvements

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -13,6 +13,25 @@ describe 'Checkout Process', type: :feature do
       it 'displays the products in the cart' do
         expect(page).to have_content 'TicketBase generic ticket'
       end
+
+      it 'displays the quantity of the product' do
+        expect(page).to have_content '1 ×'
+      end
+
+      it 'displays the total of the cart' do
+        expect(page).to have_content 'Total P5.00'
+      end
+
+      context 'and the user presses the Empty Cart button' do
+        before do
+          click_button 'Empty cart'
+          page.driver.accept_js_confirms!
+        end
+
+        it 'empties the cart', js: true do
+          expect(page).to have_content 'Cart was successfully emptied.'
+        end
+      end
     end
 
     context 'when there are multiple products in the store' do
